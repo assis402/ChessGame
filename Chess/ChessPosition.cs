7edacbf9adced5af69efe199c262001ce9a2b32e
Matheus de Assis <assis4002@gmail.com>
2020-10-30 21:03:21 -0300

    ChessPosition class

---
+++ Chess/ChessPosition.cs
@@ -0,0 +1,28 @@
+﻿using ChessBoard;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Chess
+{
+    class ChessPosition
+    {
+        public char row { get; set; }
+        public int line { get; set; }
+
+        public ChessPosition(char row, int line)
+        {
+            this.row = row;
+            this.line = line;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - line, row - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + row + line;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,21 +8,11 @@ namespace ChessGame
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board board = new Board(8, 8);
+            ChessPosition pos = new ChessPosition('a', 1);
 
-                board.putPiece(new Tower(board, Color.Black), new Position(0, 0));
-                board.putPiece(new Tower(board, Color.Black), new Position(1, 9));
-                board.putPiece(new King(board, Color.Black), new Position(0, 2));
+            Console.WriteLine(pos);
 
-                Screen.printBoard(board);
-            }
-
-            catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.toPosition());
 
             Console.ReadLine();
         }
